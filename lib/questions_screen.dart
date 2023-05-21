c16a47e7fee0878da6d428f128a98223ca33f730
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-05-20 22:18:21 -0400

    Changes to be committed: 	modified:   lib/questions_screen.dart
    Changes not staged for commit:
    	modified:   lib/data/questions.dart

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -10,7 +10,7 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
+    return Column(children: const [
       Text('The question...'),
     ]);
   }
