ed4ece0359edcdab1e35634823774020bf73e1b0
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-05-30 22:12:37 -0400

    modified:   lib/questions_summer.dart

--- lib/questions_summer.dart
+++ lib/questions_summer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsSummary extends StatelessWidget {
   const QuestionsSummary({super.key, required this.summaryData});
@@ -11,7 +12,19 @@ class QuestionsSummary extends StatelessWidget {
       children: summaryData.map((data) {
         return Row(
           children: [
-            Text(((data['question'] as int) + 1).toString()),
+            Text(((data['question_index'] as int) + 1).toString()),
+            Expanded(
+              child: Column(
+                children: [
+                  Text(data['questions'] as String),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(data['user_answer'] as String),
+                  Text(data['correct_answer'] as String),
+                ],
+              ),
+            ),
           ],
         );
       }).toList(),
